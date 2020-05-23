7ec10d1fd820b1a951ff9e8fff3cfe0db7103048
Kapyia <43840453+Kapyia@users.noreply.github.com>
2020-05-23 22:44:28 +0200

    Uploaded script

---
+++ rmWhiteBorder/rmwhiteborder/rmwhiteborder.py
@@ -0,0 +1,18 @@
+from krita import *
+
+class RmWhiteBorder(Extension):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def setup(self):
+        pass
+
+    def createActions(self, window):
+        Krita.instance().activeWindow().qwindow().setStyleSheet("""
+            QToolBar {
+                border: none;
+            }
+        """)
+        
+Krita.instance().addExtension(RmWhiteBorder(Krita.instance()))
